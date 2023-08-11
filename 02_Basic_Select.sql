887204452c7612db483ae03759c7ae4ac1dfb1a6
Aroom Gonzalez <aroom.dev@gmail.com>
2023-08-11 12:07:52 -0500

    Update and rename Basic Select.sql to 02_Basic_Select.sql

---
+++ 02_Basic_Select.sql
@@ -0,0 +1,3 @@
+-- #############################################################################################################################################################
+-- Revising the Select Query I
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
